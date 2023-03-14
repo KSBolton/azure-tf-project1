c61f1e66f56464de06f6f647f204ec738da42009
KSBolton <ksbolton@gmail.com>
2023-03-14 18:46:30 -0400

    linux and windows vms created

---
+++ modules/compute/vm.tf
@@ -0,0 +1,54 @@
+resource "azurerm_linux_virtual_machine" "lnx_vms" {
+  for_each              = var.vm_names
+  name                  = "${var.student_id}-${each.key}"
+  location              = startswith(each.key, "r1") ? var.us_rg_loc : startswith(each.key, "r2") ? var.eu_rg_loc : ""
+  resource_group_name   = startswith(each.key, "r1") ? var.us_rg_name : startswith(each.key, "r2") ? var.eu_rg_name : ""
+  network_interface_ids = [azurerm_network_interface.vm_nics[each.key].id]
+  size                  = "Standard_B2s"
+
+  os_disk {
+    name                 = "${each.key}-os-disk"
+    caching              = "ReadWrite"
+    storage_account_type = "StandardSSD_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+
+  computer_name                   = "${var.student_id}-${each.key}"
+  admin_username                  = "kbolton3"
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = "kbolton3"
+    public_key = tls_private_key.vm_key.public_key_openssh
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "client_vms" {
+  for_each              = var.client_names
+  name                  = each.key
+  location              = endswith(each.key, "r1") ? var.us_rg_loc : endswith(each.key, "r2") ? var.eu_rg_loc : ""
+  resource_group_name   = endswith(each.key, "r1") ? var.us_rg_name : endswith(each.key, "r2") ? var.eu_rg_name : ""
+  size                  = "Standard_B2s"
+  admin_username        = "kbolton3"
+  admin_password        = var.client_vm_pwd
+  network_interface_ids = [azurerm_network_interface.client_nics[each.key].id]
+  computer_name         = replace(each.key, "_", "-")
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsDesktop"
+    offer     = "windows-11"
+    sku       = "win11-22h2-pro"
+    version   = "latest"
+  }
+}
