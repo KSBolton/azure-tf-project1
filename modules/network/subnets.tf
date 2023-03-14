0678f3fda98ad3eac73795de255b1233ff44fe6e
KSBolton <ksbolton@gmail.com>
2023-03-14 18:47:01 -0400

    fix vnet name declaration

--- modules/network/subnets.tf
+++ modules/network/subnets.tf
@@ -1,13 +1,13 @@
 resource "azurerm_subnet" "us_subnet" {
   name                 = "us_subnet1"
   resource_group_name  = var.us_rg_name
-  virtual_network_name = azurerm_virtual_network.usa_net
+  virtual_network_name = azurerm_virtual_network.usa_net.name
   address_prefixes     = ["10.0.3.0/27"]
 }
 
 resource "azurerm_subnet" "eu_subnet" {
   name                 = "eu_subnet1"
   resource_group_name  = var.eu_rg_name
-  virtual_network_name = azurerm_virtual_network.euro_net
+  virtual_network_name = azurerm_virtual_network.euro_net.name
   address_prefixes     = ["10.0.3.64/27"]
 }
