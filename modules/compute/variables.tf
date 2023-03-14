0a010ba18cb10424ba1c2c6dbc9781589c32e800
KSBolton <ksbolton@gmail.com>
2023-03-14 18:45:46 -0400

    adding client_names list

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -5,8 +5,14 @@ variable "eu_rg_loc" {}
 variable "us_subnet1_id" {}
 variable "eu_subnet1_id" {}
 variable "student_id" {}
+variable "client_vm_pwd" {}
 
 variable "vm_names" {
-  type    = list(string)
+  type    = set(string)
   default = ["r1-VM1", "r1-VM2", "r2-VM3", "r2-VM4"]
+}
+
+variable "client_names" {
+  type    = set(string)
+  default = ["Client_vm_r1", "Client_vm_r2"]
 }
