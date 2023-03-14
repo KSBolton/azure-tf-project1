46b8b5f1cbfd815a4c70c666f91a100447f9f988
KSBolton <ksbolton@gmail.com>
2023-03-14 18:44:47 -0400

    creating NICs

--- modules/compute/nic.tf
+++ modules/compute/nic.tf
@@ -1,12 +1,25 @@
-resource "azurerm_network_interface" "name" {
+resource "azurerm_network_interface" "vm_nics" {
   for_each            = var.vm_names
-  name                = "${each.value}-nic1"
-  location            = startswith(each.value, "r1") ? us_rg_loc : startswith(each.value, "r2") ? eu_rg_loc : ""
-  resource_group_name = startswith(each.value, "r1") ? us_rg_name : startswith(each.value, "r2") ? eu_rg_name : ""
+  name                = "${each.value}_nic1"
+  location            = startswith(each.value, "r1") ? var.us_rg_loc : startswith(each.value, "r2") ? var.eu_rg_loc : ""
+  resource_group_name = startswith(each.value, "r1") ? var.us_rg_name : startswith(each.value, "r2") ? var.eu_rg_name : ""
 
   ip_configuration {
-    name               = "internal"
-    subnet_id          = startswith(each.value, "r1") ? us_subnet1_id : startswith(each.value, "r2") ? eu_subnet1_id : ""
-    private_ip_address = "Dynamic"
+    name                          = "internal"
+    subnet_id                     = startswith(each.value, "r1") ? var.us_subnet1_id : startswith(each.value, "r2") ? var.eu_subnet1_id : ""
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_network_interface" "client_nics" {
+  for_each            = var.client_names
+  name                = "${each.value}-nic1"
+  location            = endswith(each.value, "r1") ? var.us_rg_loc : endswith(each.value, "r2") ? var.eu_rg_loc : ""
+  resource_group_name = endswith(each.value, "r1") ? var.us_rg_name : endswith(each.value, "r2") ? var.eu_rg_name : ""
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = endswith(each.value, "r1") ? var.us_subnet1_id : endswith(each.value, "r2") ? var.eu_subnet1_id : ""
+    private_ip_address_allocation = "Dynamic"
   }
 }
