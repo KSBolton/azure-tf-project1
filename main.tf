fca5e8acdc96e6be848bd6d308dca0e9d438eb8d
KSBolton <ksbolton@gmail.com>
2023-03-15 16:25:24 -0400

    load balancer module implemented

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ module "keyvault" {
   source = "./modules/keyvault"
 }
 
-module "vms" {
+module "compute" {
   source        = "./modules/compute"
   us_subnet1_id = module.vnet.us_subnet1_id
   eu_subnet1_id = module.vnet.eu_subnet1_id
@@ -28,6 +28,19 @@ module "vms" {
   us_rg_loc     = azurerm_resource_group.proj1_westus.location
   eu_rg_name    = azurerm_resource_group.proj1_westeuro.name
   eu_rg_loc     = azurerm_resource_group.proj1_westeuro.location
-  student_id    = var.student_id
+  prefix    = var.prefix
   client_vm_pwd = module.keyvault.client_vm_pwd
+  lnx_vm_config = var.lnx_vm_config
+  client_vm_config = var.client_vm_config
+}
+
+module "loadbalancer" {
+  source = "./modules/loadbalancer"
+  us_subnet1_id = module.vnet.us_subnet1_id
+  eu_subnet1_id = module.vnet.eu_subnet1_id
+  us_rg_name    = azurerm_resource_group.proj1_westus.name
+  us_rg_loc     = azurerm_resource_group.proj1_westus.location
+  eu_rg_name    = azurerm_resource_group.proj1_westeuro.name
+  eu_rg_loc     = azurerm_resource_group.proj1_westeuro.location  
+  lnx_vm_nics = module.compute.lnx_vm_nics
 }
