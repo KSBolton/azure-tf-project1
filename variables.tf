d0df55d3b08564e19791b7e50c059b319253bb7f
KSBolton <ksbolton@gmail.com>
2023-03-15 16:26:50 -0400

    best practice changes

--- variables.tf
+++ variables.tf
@@ -1,3 +1,23 @@
-variable "student_id" {
+variable "prefix" {
   default = "kbolton3"
+}
+
+variable "lnx_vm_config" {
+  type = map(string)
+  default = {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+}
+
+variable "client_vm_config" {
+  type = map(string)
+  default = {
+    publisher = "MicrosoftWindowsDesktop"
+    offer     = "windows-11"
+    sku       = "win11-22h2-pro"
+    version   = "latest"
+  }
 }
